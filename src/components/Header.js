d3fdce68ab584b46b04475d11264aff4d79ad424
AleksanderTelecki <alexanderteleckiy@gmail.com>
2022-03-26 00:06:02 +0100

    Implementing react Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import {Navbar,Nav,Container} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 function Header(props) {
     return (
         <header>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">
-                        <i className="fa-solid fa-book-open"></i>
-                        Multum Librorum
-                    </Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>
+                            <i className="fa-solid fa-book-open"></i>
+                            Multum Librorum
+                        </Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
 
                         </Nav>
                         <Nav className="mr-auto">
-                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-                            <Nav.Link href="/login">
-                                <i className="fas fa-user"></i>Login
-                            </Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link>
+                                    <i className="fas fa-user"></i>Login
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
