f968085bbc7e03c0f79ed4f614db62fa9ba6a5d7
AleksanderTelecki <alexanderteleckiy@gmail.com>
2022-03-25 16:06:19 +0100

    Header and Footer Components

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Navbar,Nav,Container} from 'react-bootstrap'
+
+function Header(props) {
+    return (
+        <header>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">Multum Librorum</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+
+                        </Nav>
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                            <Nav.Link href="/login">
+                                <i className="fas fa-user"></i>Login
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+}
+
+export default Header;
